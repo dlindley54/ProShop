487621ee93b5066baf4e7b358a5bcb089a4dceed
dlindley54 <dlindley5454@gmail.com>
2022-09-26 15:00:09 -0500

    Added backend API to the project

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Nav, Navbar, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+
+import { LinkContainer } from 'react-router-bootstrap';
 
 function Header() {
 	return (
 		<header>
 			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect fixed='top'>
 				<Container>
-					<Navbar.Brand as={Link} to='/'>
-						ProShop
-					</Navbar.Brand>
+					<LinkContainer to='/'>
+						<Navbar.Brand>ProShop</Navbar.Brand>
+					</LinkContainer>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='ms-auto'>
-							<Nav.Link as={Link} to='/cart'>
-								<i className='fas fa-shopping-cart'></i> Cart
-							</Nav.Link>
-							<Nav.Link as={Link} to='/login'>
-								<i className='fa fa-user'></i> Sign In
-							</Nav.Link>
+							<LinkContainer to='/cart'>
+								<Nav.Link>
+									<i className='fas fa-shopping-cart'></i> Cart
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/login'>
+								<Nav.Link>
+									<i className='fa fa-user'></i> Sign In
+								</Nav.Link>
+							</LinkContainer>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
